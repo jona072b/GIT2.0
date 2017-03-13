1b47be07b5b5e7f2029a536cb51c9c364ec527dd
Jonathan Hansen <jona072b@stud.kea.dk>
2017-03-13 12:56:30 +0000

    Hej Alberte

--- src/Car.java
+++ src/Car.java
@@ -2,6 +2,12 @@
  * Created by Alberte on 13/03/2017.
  */
 public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+        System.out.println("Now it is a car");
+    }
 
     public Car() {
         System.out.println("This is not a car");
